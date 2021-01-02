7a077c1f1aaaa2be0291dae0e2b347583a10c39f
Illia Grymalo <illia050@ukr.net>
2021-01-02 17:00:45 +0200

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  var mySwiper = new Swiper('.edge-slider', {
+    pagination: {
+      el: '.swiper-pagination'
+    }
+  });
+  // $('.edge-slider__items').slick({
+  //   dots: true,
+  //   arrows: false
+  // });
+});
